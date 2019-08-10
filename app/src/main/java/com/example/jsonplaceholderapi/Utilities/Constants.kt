7ba78ed905d3b537c6874c0d55008972867b5cfd
afce030 <andrescardona056@hotmail.com>
2019-08-10 14:03:45 -0500

    Constants

--- app/src/main/java/com/example/jsonplaceholderapi/Utilities/Constants.kt
+++ app/src/main/java/com/example/jsonplaceholderapi/Utilities/Constants.kt
@@ -1,5 +1,5 @@
 package com.example.jsonplaceholderapi.Utilities
 
 object Constants {
-    const val BASE_URL = "https://jsonplaceholder.typicode.com/";
+    const val BASE_URL = "https://jsonplaceholder.typicode.com/"
 }
