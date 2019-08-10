b6d2f0b107959e546e30343d9c5a47fd40a65551
afce030 <andrescardona056@hotmail.com>
2019-08-10 13:40:04 -0500

    Creating Dagger module

--- app/src/main/java/com/example/jsonplaceholderapi/di/qualifiers/ViewModelInjection.kt
+++
@@ -1,9 +0,0 @@
-package com.example.jsonplaceholderapi.di.qualifiers
-
-import javax.inject.Qualifier
-
-@Qualifier
-@MustBeDocumented
-@Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.FIELD, AnnotationTarget.FUNCTION)
-annotation class ViewModelInjection
--- app/src/main/java/com/example/jsonplaceholderapi/ui/BaseViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.jsonplaceholderapi.ui
-
-import android.app.Application
-import android.arch.lifecycle.ViewModel
-import io.reactivex.disposables.CompositeDisposable
-
-abstract class BaseViewModel() : ViewModel() {
-    val compositeDisposable = CompositeDisposable()
-}
