9e932d437aec85dd9af535db04da721f9762b974
afce030 <andrescardona056@hotmail.com>
2019-08-10 18:52:46 -0500

    Creating the post database

---
+++ app/src/main/java/com/example/jsonplaceholderapi/LocalData/RoomDatabases/PostDatabase.kt
@@ -0,0 +1,26 @@
+package com.example.jsonplaceholderapi.LocalData.RoomDatabases
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.jsonplaceholderapi.LocalData.RoomEntities.PostEntity
+
+@Database(entities = [PostEntity::class], version = 1, exportSchema = false)
+abstract class PostDatabase: RoomDatabase() {
+
+    //Singleton with context as parameter
+    companion object {
+        var instance: PostDatabase? = null
+
+        fun getPostDataBase(context: Context): PostDatabase? {
+            if (instance == null) {
+                synchronized(PostDatabase::class) {
+                    instance =
+                        Room.databaseBuilder(context.applicationContext, PostDatabase::class.java, "PostDB").build()
+                }
+            }
+            return instance
+        }
+    }
+}
