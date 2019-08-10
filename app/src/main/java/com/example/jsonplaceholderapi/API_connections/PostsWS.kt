1150272d48ac9c05e612ecbce1d800fb5e5b9d85
afce030 <andrescardona056@hotmail.com>
2019-08-10 12:16:23 -0500

    Retrofit connection file

---
+++ app/src/main/java/com/example/jsonplaceholderapi/API_connections/PostsWS.kt
@@ -0,0 +1,13 @@
+package com.example.jsonplaceholderapi.API_connections
+
+import com.example.jsonplaceholderapi.POJO_classes.PostDTO
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface PostsWS {
+
+    @GET("post/{id}")
+    fun getPostByID(@Path("id") number: Int) : Call<PostDTO>
+
+}
