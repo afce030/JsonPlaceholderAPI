43cdda7df2ab953ee831b7b743581ae5ba27c088
afce030 <andrescardona056@hotmail.com>
2019-08-10 15:02:59 -0500

    Module finished

--- app/src/main/java/com/example/jsonplaceholderapi/di/DaggerModules/RetrofitModule.kt
+++ app/src/main/java/com/example/jsonplaceholderapi/di/DaggerModules/RetrofitModule.kt
@@ -1,10 +1,12 @@
 package com.example.jsonplaceholderapi.di.DaggerModules
 
+import com.example.jsonplaceholderapi.API_connections.PostsWS
 import com.example.jsonplaceholderapi.Utilities.Constants
 import dagger.Module
 import dagger.Provides
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 @Module
 object RetrofitModule {
@@ -16,12 +18,15 @@ object RetrofitModule {
 
     @Provides
     fun providesRetrofitService(gson: GsonConverterFactory): Retrofit {
-
         return Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
             .addConverterFactory(gson)
             .build()
+    }
 
+    @Provides
+    fun providePosts(retrofit: Retrofit): PostsWS{
+        return retrofit.create(PostsWS::class.java)
     }
 
 }
