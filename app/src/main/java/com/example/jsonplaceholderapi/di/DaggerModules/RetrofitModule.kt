9d3aca9d0f6cb0a59ae32f15a472c1f945aed4cd
afce030 <andrescardona056@hotmail.com>
2019-08-10 13:40:04 -0500

    Creating Dagger module

---
+++ app/src/main/java/com/example/jsonplaceholderapi/di/DaggerModules/RetrofitModule.kt
@@ -0,0 +1,25 @@
+package com.example.jsonplaceholderapi.di.DaggerModules
+
+import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+object RetrofitModule {
+
+    @Provides
+    fun provideGsonConverter(): GsonConverterFactory{
+        return GsonConverterFactory.create()
+    }
+
+    @Provides
+    fun providesRetrofitService(gson: GsonConverterFactory): Retrofit {
+
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .build()
+
+    }
+
+}
