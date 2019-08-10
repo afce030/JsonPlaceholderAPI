69f846c82db74ab94de6b51c85c8ce00e8395728
afce030 <andrescardona056@hotmail.com>
2019-08-10 12:31:23 -0500

    Creating MVVM for kotlin

---
+++ app/src/main/java/com/example/jsonplaceholderapi/di/components/AppComponent.kt
@@ -0,0 +1,32 @@
+package com.example.jsonplaceholderapi.di.components
+
+import com.example.jsonplaceholderapi.App
+import com.example.jsonplaceholderapi.di.modules.ActivityInjectorsModule
+import com.example.jsonplaceholderapi.di.modules.FragmentInjectorsModule
+import com.example.jsonplaceholderapi.di.modules.AppModule
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.support.AndroidSupportInjectionModule
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+    modules = [
+        AndroidSupportInjectionModule::class,
+        ActivityInjectorsModule::class,
+        FragmentInjectorsModule::class,
+        AppModule::class]
+)
+interface AppComponent {
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: App): Builder
+
+        fun build(): AppComponent
+    }
+
+    fun inject(app: App)
+
+}
---
+++ app/src/main/java/com/example/jsonplaceholderapi/di/modules/ActivityInjectorsModule.kt
@@ -0,0 +1,9 @@
+package com.example.jsonplaceholderapi.di.modules
+
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+@Module
+abstract class ActivityInjectorsModule {
+
+}
