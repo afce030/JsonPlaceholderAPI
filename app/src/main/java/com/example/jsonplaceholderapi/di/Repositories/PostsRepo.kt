986ee005a2985fad60139401e3bd9df912239a66
afce030 <andrescardona056@hotmail.com>
2019-08-10 19:41:13 -0500

    Creating request

--- app/src/main/java/com/example/jsonplaceholderapi/di/Repositories/PostsRepo.kt
+++ app/src/main/java/com/example/jsonplaceholderapi/di/Repositories/PostsRepo.kt
@@ -1,9 +1,16 @@
 package com.example.jsonplaceholderapi.di.Repositories
 
+import androidx.lifecycle.LiveData
 import com.example.jsonplaceholderapi.API_connections.PostsWS
+import com.example.jsonplaceholderapi.LocalData.RoomDAO.PostsDAO
+import com.example.jsonplaceholderapi.LocalData.RoomEntities.PostEntity
+import com.example.jsonplaceholderapi.POJO_classes.PostDTO
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import javax.inject.Inject
 
-class PostsRepo {
+class PostsRepo(postsDAO: PostsDAO) {
 
     @Inject
     lateinit var postsWS: PostsWS//This parameter is injected by the Retrofit Module
@@ -11,6 +18,28 @@ class PostsRepo {
     //Creating singleton
     companion object {
         val instance = PostsRepo()
+    }
+
+    fun getPostByIDS(id: Int): LiveData<List<PostEntity>>{
+        refreshPosts(id)
+        return
+    }
+
+    fun refreshPosts(id: Int){
+        val call = postsWS.getPostByID(id)
+        call.enqueue(object : Callback<PostDTO>{
+            override fun onResponse(call: Call<PostDTO>, response: Response<PostDTO>) {
+
+                if(response.isSuccessful){
+
+                    val posts: List<PostDTO> = response.body()
+
+                }
+
+            }
+            override fun onFailure(call: Call<PostDTO>, t: Throwable) {
+            }
+        })
     }
 
 }
