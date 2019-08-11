3853d7c697c14cf337e8db4f67699c404378879d
afce030 <andrescardona056@hotmail.com>
2019-08-10 20:48:35 -0500

    Singleton for repo using parameters

--- app/src/main/java/com/example/jsonplaceholderapi/di/Repositories/PostsRepo.kt
+++ app/src/main/java/com/example/jsonplaceholderapi/di/Repositories/PostsRepo.kt
@@ -1,8 +1,11 @@
 package com.example.jsonplaceholderapi.di.Repositories
 
+import android.content.Context
 import androidx.lifecycle.LiveData
+import androidx.room.Room
 import com.example.jsonplaceholderapi.API_connections.PostsWS
 import com.example.jsonplaceholderapi.LocalData.RoomDAO.PostsDAO
+import com.example.jsonplaceholderapi.LocalData.RoomDatabases.PostDatabase
 import com.example.jsonplaceholderapi.LocalData.RoomEntities.PostEntity
 import com.example.jsonplaceholderapi.POJO_classes.PostDTO
 import retrofit2.Call
@@ -17,12 +20,23 @@ class PostsRepo(var postsDAO: PostsDAO, var foundPost: LiveData<List<PostEntity>
 
     //Creating singleton
     companion object {
-        val instance = PostsRepo()
+        var instance: PostsRepo? = null
+
+        fun getRepoInstance(PostsDAO: PostsDAO, FoundPost: LiveData<List<PostEntity>>): PostsRepo? {
+            if (instance == null) {
+                synchronized(PostsRepo::class) {
+                    instance = PostsRepo(PostsDAO, FoundPost)
+                }
+            }
+            return instance
+        }
+
     }
 
     fun getPostByIDS(id: Int): LiveData<List<PostEntity>>{
         refreshPosts(id)
-        return postsDAO.getAllWords()
+        val data: LiveData<List<PostEntity>> = postsDAO.getAllWords()
+        return data
     }
 
     fun refreshPosts(id: Int){
