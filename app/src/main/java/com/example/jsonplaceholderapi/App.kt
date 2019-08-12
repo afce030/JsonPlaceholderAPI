32e0bf982b8f37c1d3e132eba422906290505b99
afce030 <andrescardona056@hotmail.com>
2019-08-11 23:10:57 -0500

    Creating App class

---
+++ app/src/main/java/com/example/jsonplaceholderapi/App.kt
@@ -0,0 +1,17 @@
+package com.example.jsonplaceholderapi
+
+import android.app.Application
+import com.example.jsonplaceholderapi.di.DaggerComponents.DaggerRetrofitComponent
+import com.example.jsonplaceholderapi.di.DaggerComponents.RetrofitComponent
+import com.example.jsonplaceholderapi.di.DaggerModules.RetrofitModule
+
+class App : Application() {
+
+    lateinit var component: RetrofitComponent
+
+    override fun onCreate() {
+        super.onCreate()
+        component = DaggerRetrofitComponent.builder().build()
+    }
+
+}
--- app/src/main/java/com/example/jsonplaceholderapi/di/DaggerModules/RetrofitModule.kt
+++ app/src/main/java/com/example/jsonplaceholderapi/di/DaggerModules/RetrofitModule.kt
@@ -7,16 +7,19 @@ import dagger.Provides
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
+import javax.inject.Singleton
 
 @Module
 object RetrofitModule {
 
     @Provides
+    @Singleton
     fun provideGsonConverter(): GsonConverterFactory{
         return GsonConverterFactory.create()
     }
 
     @Provides
+    @Singleton
     fun providesRetrofitService(gson: GsonConverterFactory): Retrofit {
         return Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
@@ -25,6 +28,7 @@ object RetrofitModule {
     }
 
     @Provides
+    @Singleton
     fun providePosts(retrofit: Retrofit): PostsWS{
         return retrofit.create(PostsWS::class.java)
     }
